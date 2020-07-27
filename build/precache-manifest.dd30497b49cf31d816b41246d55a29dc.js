af2e2ccaafd7035a4f87b5f0da4235336d7a224c
Vanessa Castro <vanessacastrol16@gmail.com>
2020-07-26 21:35:11 -0700

    Add files via upload

---
+++ build/precache-manifest.dd30497b49cf31d816b41246d55a29dc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bea9caede6a20ede5c9861c6a714a7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0323f85b8ef380cccd4f",
+    "url": "/static/css/main.466f7622.chunk.css"
+  },
+  {
+    "revision": "20f682600f3427846bf2",
+    "url": "/static/js/2.b4c69cdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4c69cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0323f85b8ef380cccd4f",
+    "url": "/static/js/main.2bb9d9a7.chunk.js"
+  },
+  {
+    "revision": "affdc8648b31ecc31c18",
+    "url": "/static/js/runtime-main.25af6fb3.js"
+  },
+  {
+    "revision": "4895fd22b12d03398814a3ae1d966262",
+    "url": "/static/media/atmosphere.4895fd22.svg"
+  },
+  {
+    "revision": "ee62dea9f0769c488e9630f52ee288f5",
+    "url": "/static/media/clouds.ee62dea9.svg"
+  },
+  {
+    "revision": "425a2644e2584f241fddf022076368d8",
+    "url": "/static/media/drizzle.425a2644.svg"
+  },
+  {
+    "revision": "d1b5610fb19da5198f4cc13a7aa7f542",
+    "url": "/static/media/rain.d1b5610f.svg"
+  },
+  {
+    "revision": "0d8e2905457f19c2176a1e374f3936d1",
+    "url": "/static/media/snow.0d8e2905.svg"
+  },
+  {
+    "revision": "21c2f7e346f2e8304e52c64ebf42740f",
+    "url": "/static/media/thunderstorm.21c2f7e3.svg"
+  },
+  {
+    "revision": "e1bb2cc73021b3967272fca53a84621f",
+    "url": "/static/media/warm.e1bb2cc7.svg"
+  }
+]);
